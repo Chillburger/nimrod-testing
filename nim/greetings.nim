146bd9e4e97357d4444b5bdb0ab822c0e657527e
root <root@pass0655.na.sas.com>
2015-01-30 19:24:08 -0500

    adding changes for greeting and literal character experimentation

--- nim/greetings.nim
+++ nim/greetings.nim
@@ -3,4 +3,8 @@
 echo ("Hello, what is your name? ")
 
 var name: string = readLine(stdin)
+
+var literal: string = r"C:\program files\nim"
 echo ("Hi, my name is ", name , "!")
+echo ("\n")
+echo ("\t" , literal)
